7e05ee70e66cba1421ffed58e6dfb4e936828394
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-12-10 01:18:00 +0100

    Checkpoint

--- src/components/results/TableView.tsx
+++ src/components/results/TableView.tsx
@@ -1,47 +1,56 @@
-import type { EducationCategory } from "../../util/contestResults.ts";
-import type { FC } from "react";
+import type { ContestTableEntry, EducationCategory } from "../../util/contestResults.ts";
+import { type FC, useState } from "react";
+import "./table-styles.scss";
 
 type Properties = {
     dataByCategory: Record<EducationCategory | "all", ContestTableEntry[]>;
 }
 
-export const TableView: FC<EducationCategory> = ({ dataByCategory }: Properties) => {
+export const TableView: FC<Properties> = ({ dataByCategory }: Properties) => {
+    const [ selectedCategory, setSelectedCategory ] = useState<EducationCategory | "all">("all");
+
     return (
-        <table>
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Ime i prezime</th>
-                <th>Kategorija</th>
-                <th>Institucija</th>
-                <th>Kontestis</th>
-                <th>Testiranje</th>
-                <th>Komunikacija</th>
-                <th>Teta</th>
-                <th>Donald</th>
-                <th>Kvadrati</th>
-                <th>Lozinka</th>
-                <th>Ukupno</th>
-            </tr>
-            </thead>
-            <tbody>
-            {DataByCategory["all"].map(it => (
+        <div className={"container"}>
+            <button onClick={() => setSelectedCategory("all")}>All</button>
+            <button onClick={() => setSelectedCategory("university")}>Uni</button>
+            <button onClick={() => setSelectedCategory("high")}>High</button>
+            <button onClick={() => setSelectedCategory("elementary")}>Elementary</button>
+            <table>
+                <thead>
                 <tr>
-                    <td>{it.rank}</td>
-                    <td>{it.full_name}</td>
-                    <td>{it.category}</td>
-                    <td>{it.institution}</td>
-                    <td>{it.score["kontestis"] ?? ""}</td>
-                    <td>{it.score["testiranje"] ?? ""}</td>
-                    <td>{it.score["komunikacija"] ?? ""}</td>
-                    <td>{it.score["teta"] ?? ""}</td>
-                    <td>{it.score["donald"] ?? ""}</td>
-                    <td>{it.score["kvadrati"] ?? ""}</td>
-                    <td>{it.score["lozinka"] ?? ""}</td>
-                    <td>{it.total}</td>
+                    <th>#</th>
+                    <th>Ime i prezime</th>
+                    <th>Kategorija</th>
+                    <th>Institucija</th>
+                    <th>Kontestis</th>
+                    <th>Testiranje</th>
+                    <th>Komunikacija</th>
+                    <th>Teta</th>
+                    <th>Donald</th>
+                    <th>Kvadrati</th>
+                    <th>Lozinka</th>
+                    <th>Ukupno</th>
                 </tr>
-            ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                {dataByCategory[selectedCategory].map((it, index) => (
+                    <tr key={index.toString()}>
+                        <td>{it.rank}</td>
+                        <td>{it.full_name}</td>
+                        <td>{it.category}</td>
+                        <td>{it.institution}</td>
+                        <td>{it.score["kontestis"] ?? ""}</td>
+                        <td>{it.score["testiranje"] ?? ""}</td>
+                        <td>{it.score["komunikacija"] ?? ""}</td>
+                        <td>{it.score["teta"] ?? ""}</td>
+                        <td>{it.score["donald"] ?? ""}</td>
+                        <td>{it.score["kvadrati"] ?? ""}</td>
+                        <td>{it.score["lozinka"] ?? ""}</td>
+                        <td>{it.total}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
