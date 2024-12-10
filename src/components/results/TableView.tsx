5666d537554294550f071ee86171023b5eabc1c5
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-12-10 01:49:57 +0100

    Finish basic results table view

--- src/components/results/TableView.tsx
+++ src/components/results/TableView.tsx
@@ -11,46 +11,58 @@ export const TableView: FC<Properties> = ({ dataByCategory }: Properties) => {
 
     return (
         <div className={"container"}>
-            <button onClick={() => setSelectedCategory("all")}>All</button>
-            <button onClick={() => setSelectedCategory("university")}>Uni</button>
-            <button onClick={() => setSelectedCategory("high")}>High</button>
-            <button onClick={() => setSelectedCategory("elementary")}>Elementary</button>
-            <table>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Ime i prezime</th>
-                    <th>Kategorija</th>
-                    <th>Institucija</th>
-                    <th>Kontestis</th>
-                    <th>Testiranje</th>
-                    <th>Komunikacija</th>
-                    <th>Teta</th>
-                    <th>Donald</th>
-                    <th>Kvadrati</th>
-                    <th>Lozinka</th>
-                    <th>Ukupno</th>
-                </tr>
-                </thead>
-                <tbody>
-                {dataByCategory[selectedCategory].map((it, index) => (
-                    <tr key={index.toString()}>
-                        <td>{it.rank}</td>
-                        <td>{it.full_name}</td>
-                        <td>{it.category}</td>
-                        <td>{it.institution}</td>
-                        <td>{it.score["kontestis"] ?? ""}</td>
-                        <td>{it.score["testiranje"] ?? ""}</td>
-                        <td>{it.score["komunikacija"] ?? ""}</td>
-                        <td>{it.score["teta"] ?? ""}</td>
-                        <td>{it.score["donald"] ?? ""}</td>
-                        <td>{it.score["kvadrati"] ?? ""}</td>
-                        <td>{it.score["lozinka"] ?? ""}</td>
-                        <td>{it.total}</td>
+            <div className={"buttons"}>
+                <button onClick={() => setSelectedCategory("all")}
+                        className={selectedCategory === "all" && "active"}>Svi
+                </button>
+                <button onClick={() => setSelectedCategory("university")}
+                        className={selectedCategory === "university" && "active"}>Fakulteti
+                </button>
+                <button onClick={() => setSelectedCategory("high")}
+                        className={selectedCategory === "high" && "active"}>Srednje škole
+                </button>
+                <button onClick={() => setSelectedCategory("elementary")}
+                        className={selectedCategory === "elementary" && "active"}>Osnovne škole
+                </button>
+            </div>
+            <div className={"table-container"}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Ime i prezime</th>
+                        <th>Kategorija</th>
+                        <th>Institucija</th>
+                        <th>Kontestis</th>
+                        <th>Testiranje</th>
+                        <th>Komunikacija</th>
+                        <th>Teta</th>
+                        <th>Donald</th>
+                        <th>Kvadrati</th>
+                        <th>Lozinka</th>
+                        <th>Ukupno</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {dataByCategory[selectedCategory].map((it, index) => (
+                        <tr key={index.toString()}>
+                            <td className={"center"}>{it.rank}</td>
+                            <td>{it.full_name}</td>
+                            <td>{it.category}</td>
+                            <td>{it.institution}</td>
+                            <td className={"center"}>{it.score["kontestis"] ?? ""}</td>
+                            <td className={"center"}>{it.score["testiranje"] ?? ""}</td>
+                            <td className={"center"}>{it.score["komunikacija"] ?? ""}</td>
+                            <td className={"center"}>{it.score["teta"] ?? ""}</td>
+                            <td className={"center"}>{it.score["donald"] ?? ""}</td>
+                            <td className={"center"}>{it.score["kvadrati"] ?? ""}</td>
+                            <td className={"center"}>{it.score["lozinka"] ?? ""}</td>
+                            <td className={"center"}>{it.total}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
