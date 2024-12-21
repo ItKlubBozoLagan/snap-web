b278778fe3fd4b4e043280fa48bfd84b5e9a2d91
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2024-12-21 23:27:24 +0100

    Uplaod more data

---
+++ src/components/results/ContestsTableView.tsx
@@ -0,0 +1,109 @@
+import type { ContestTableEntry, EducationCategory } from "../../util/contestResults.ts";
+import { type FC, useEffect, useMemo, useState } from "react";
+import "./table-styles.scss";
+
+type OneContestData = {
+    problems: string[];
+    data: Record<EducationCategory | "all", ContestTableEntry[]>;
+};
+
+type Properties = {
+    dataByContests: Record<string, OneContestData>;
+};
+
+export const ContestsView: FC<Properties> = ({ dataByContests }: Properties) => {
+    const [selectedContest, setSelectedContest] = useState<string>(Object.keys(dataByContests)[0]);
+
+    const { problems, data: dataByCategory } = useMemo(
+        () => dataByContests[selectedContest],
+        [selectedContest],
+    );
+
+    return (
+        <div className={"container"}>
+            <div className={"buttons buttons-contest"}>
+                {Object.keys(dataByContests).map((contest) => (
+                    <button
+                        onClick={() => setSelectedContest(contest)}
+                        className={selectedContest === contest ? "active" : ""}
+                    >
+                        {contest}
+                    </button>
+                ))}
+            </div>
+            <TableView problems={problems} data={dataByCategory} />
+        </div>
+    );
+};
+
+const TableView: FC<OneContestData> = ({ problems, data }: OneContestData) => {
+    const [selectedCategory, setSelectedCategory] = useState<EducationCategory | "all">("all");
+
+    useEffect(() => {
+        setSelectedCategory("all");
+    }, [problems]);
+
+    return (
+        <div className={"container"}>
+            <div className={"buttons"}>
+                <button
+                    onClick={() => setSelectedCategory("all")}
+                    className={selectedCategory === "all" ? "active" : ""}
+                >
+                    Svi
+                </button>
+                <button
+                    onClick={() => setSelectedCategory("university")}
+                    className={selectedCategory === "university" ? "active" : ""}
+                >
+                    Fakulteti
+                </button>
+                <button
+                    onClick={() => setSelectedCategory("high")}
+                    className={selectedCategory === "high" ? "active" : ""}
+                >
+                    Srednje škole
+                </button>
+                <button
+                    onClick={() => setSelectedCategory("elementary")}
+                    className={selectedCategory === "elementary" ? "active" : ""}
+                >
+                    Osnovne škole
+                </button>
+            </div>
+            <div className={"table-container"}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Ime i prezime</th>
+                            <th>Kategorija</th>
+                            <th>Institucija</th>
+                            {problems.map((problem) => (
+                                <th key={problem}>{problem}</th>
+                            ))}
+                            <th>Ukupno</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data[selectedCategory].map((it, index) => (
+                            <tr key={index.toString()}>
+                                <td className={"center"}>{it.rank}</td>
+                                <td>{it.full_name}</td>
+                                <td>{it.category}</td>
+                                <td>{it.institution}</td>
+                                {problems.map((problem) => (
+                                    <td key={problem} className={"center"}>
+                                        {it.score[problem] ?? ""}
+                                    </td>
+                                ))}
+
+                                <td className={"center"}>{it.total}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
--- src/util/contestResults.ts
+++ src/util/contestResults.ts
@@ -1,9 +1,15 @@
 export type EducationCategory = "university" | "high" | "elementary";
 
 const categoryNameMap = {
-    "university": "Fakultet",
-    "high": "Srednja škola",
-    "elementary": "Osnovna škola"
+    university: "Fakultet",
+    high: "Srednja škola",
+    elementary: "Osnovna škola",
+};
+
+export type ContestData = {
+    name: string;
+    problems: string[];
+    data: RawContestDataEntry[];
 };
 
 export type ContestTableEntry = {
@@ -13,29 +19,34 @@ export type ContestTableEntry = {
     score: Record<string, number>;
     total: number;
     rank: number;
-}
+};
 
 export type RawContestDataEntry = {
     full_name: string;
     category: string;
     institution: string;
     score: Record<string, number>;
-}
+};
 
 export const processContestResults = (data: RawContestDataEntry[]): ContestTableEntry[] => {
-    const sorted: ContestTableEntry[] = data.map(it => ({
-        ...it,
-        category: categoryNameMap[it.category],
-        total: Object.values(it.score).reduce((acc, curr) => acc + curr, 0),
-        rank: 0
-    })).sort((a, b) => b.total - a.total);
+    const sorted: ContestTableEntry[] = data
+        .map((it) => ({
+            ...it,
+            category: categoryNameMap[it.category] ?? it.category,
+            total: Object.values(it.score).reduce((acc, curr) => acc + curr, 0),
+            rank: 0,
+        }))
+        .sort((a, b) => b.total - a.total);
 
     const processed: ContestTableEntry[] = [];
 
     for (let i = 0; i < sorted.length; i++) {
         processed.push({
             ...sorted[i],
-            rank: i !== 0 && processed[i - 1].total === sorted[i].total ? processed[i - 1].rank : i + 1
+            rank:
+                i !== 0 && processed[i - 1].total === sorted[i].total
+                    ? processed[i - 1].rank
+                    : i + 1,
         });
     }
 
